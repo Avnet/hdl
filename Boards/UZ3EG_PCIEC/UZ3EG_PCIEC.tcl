4774c17c30dca3d9d4255d871106ab1ec00df092
Kevin Keryk <kevin.keryk@avnet.com>
2016-12-29 18:30:05 -0700

    Adding project files used for UltraZed PCIe Carrier Factory Acceptance Test.

---
+++ Boards/UZ3EG_PCIEC/UZ3EG_PCIEC.tcl
@@ -0,0 +1,93 @@
+# ----------------------------------------------------------------------------
+#       _____
+#      *     *
+#     *____   *____
+#    * *===*   *==*
+#   *___*===*___**  AVNET
+#        *======*
+#         *====*
+# ----------------------------------------------------------------------------
+# 
+#  This design is the property of Avnet.  Publication of this
+#  design is not authorized without written consent from Avnet.
+# 
+#  Please direct any questions to the UltraZed community support forum:
+#     http://www.ultrazed.org/forum
+# 
+#  Product information is available at:
+#     http://www.ultrazed.org/product/ultrazed
+# 
+#  Disclaimer:
+#     Avnet, Inc. makes no warranty for the use of this code or design.
+#     This code is provided  "As Is". Avnet, Inc assumes no responsibility for
+#     any errors, which may appear in this code, nor does it make a commitment
+#     to update the information contained herein. Avnet, Inc specifically
+#     disclaims any implied warranties of fitness for a particular purpose.
+#                      Copyright(c) 2016 Avnet, Inc.
+#                              All rights reserved.
+# 
+# ----------------------------------------------------------------------------
+# 
+#  Create Date:         Dec 21, 2016
+#  Design Name:         
+#  Module Name:         
+#  Project Name:        
+#  Target Devices:      Zynq UltraScale+ 3EG
+#  Hardware Boards:     UltraZed, PCIe Carrier
+# 
+#  Tool versions:       Vivado 2016.2
+# 
+#  Description:         Build Script for UltraZed PCIe Carrier
+# 
+#  Dependencies:        To be called from a project build script
+# 
+# ----------------------------------------------------------------------------
+
+proc avnet_create_project {project projects_folder scriptdir} {
+
+   create_project $project $projects_folder -part xczu3eg-sfva625-1-i-es1 -force
+   # add selection for proper xdc based on needs
+   # if IO carrier, then use that xdc
+   # if FMC, choose that one
+
+}
+
+proc avnet_add_user_io_preset {project projects_folder scriptdir} {
+
+   # this uses board automation for the UZ SOM which is derived from the 
+   # board definition file downloadable from the UltraZed.org community site.
+   create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_0
+   apply_board_connection -board_interface "dip_switches_8bits" -ip_intf "axi_gpio_0/GPIO" -diagram $project 
+   create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_1
+   apply_board_connection -board_interface "led_8bits" -ip_intf "axi_gpio_1/GPIO" -diagram $project 
+   create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_2
+   apply_board_connection -board_interface "push_buttons_3bits" -ip_intf "axi_gpio_2/GPIO" -diagram $project
+   apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_LPD" Clk "Auto" }  [get_bd_intf_pins axi_gpio_0/S_AXI]
+   # </axi_gpio_0/S_AXI/Reg> is being mapped into </zynq_ultra_ps_e_0/Data> at <0x80000000 [ 64K ]>
+   apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_LPD" Clk "Auto" }  [get_bd_intf_pins axi_gpio_1/S_AXI]
+   # </axi_gpio_1/S_AXI/Reg> is being mapped into </zynq_ultra_ps_e_0/Data> at <0x80010000 [ 64K ]>
+   apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_LPD" Clk "Auto" }  [get_bd_intf_pins axi_gpio_2/S_AXI]
+   # </axi_gpio_2/S_AXI/Reg> is being mapped into </zynq_ultra_ps_e_0/Data> at <0x80020000 [ 64K ]>
+   
+}
+
+proc avnet_add_ps_preset {project projects_folder scriptdir} {
+
+   # add selection for customization depending on board choice (or none)
+   create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:1.2 zynq_ultra_ps_e_0
+   apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1" }  [get_bd_cells zynq_ultra_ps_e_0]
+
+   set zynq_ultra_ps_e_0 [get_bd_cells zynq_ultra_ps_e_0]
+
+   # Not seeing the watchdog or TTC settings being implemented by the board 
+   # definition settings from the official 2016.2 board definition from
+   # Xilinx.  Forcing this controller to be enabled.
+   set_property -dict [list CONFIG.PSU__CSU__PERIPHERAL__ENABLE {1} CONFIG.PSU__CSU__PERIPHERAL__IO {EMIO}] [get_bd_cells zynq_ultra_ps_e_0]
+   set_property -dict [list CONFIG.PSU__SWDT0__PERIPHERAL__ENABLE {1} CONFIG.PSU__SWDT0__PERIPHERAL__IO {EMIO}] [get_bd_cells zynq_ultra_ps_e_0]
+   set_property -dict [list CONFIG.PSU__SWDT1__PERIPHERAL__ENABLE {1} CONFIG.PSU__SWDT1__PERIPHERAL__IO {EMIO}] [get_bd_cells zynq_ultra_ps_e_0]
+   set_property -dict [list CONFIG.PSU__TTC0__PERIPHERAL__ENABLE {1} CONFIG.PSU__TTC0__PERIPHERAL__IO {EMIO}] [get_bd_cells zynq_ultra_ps_e_0]
+   set_property -dict [list CONFIG.PSU__TTC1__PERIPHERAL__ENABLE {1} CONFIG.PSU__TTC1__PERIPHERAL__IO {EMIO}] [get_bd_cells zynq_ultra_ps_e_0]
+   set_property -dict [list CONFIG.PSU__TTC2__PERIPHERAL__ENABLE {1} CONFIG.PSU__TTC2__PERIPHERAL__IO {EMIO}] [get_bd_cells zynq_ultra_ps_e_0]
+   set_property -dict [list CONFIG.PSU__TTC3__PERIPHERAL__ENABLE {1} CONFIG.PSU__TTC3__PERIPHERAL__IO {EMIO}] [get_bd_cells zynq_ultra_ps_e_0]
+   
+}
---
+++ Scripts/ProjectScripts/uz_pciec_factest.tcl
@@ -0,0 +1,123 @@
+# ----------------------------------------------------------------------------
+#       _____
+#      *     *
+#     *____   *____
+#    * *===*   *==*
+#   *___*===*___**  AVNET
+#        *======*
+#         *====*
+# ----------------------------------------------------------------------------
+# 
+#  This design is the property of Avnet.  Publication of this
+#  design is not authorized without written consent from Avnet.
+# 
+#  Please direct any questions to the UltraZed community support forum:
+#     http://www.ultrazed.org/forum
+# 
+#  Product information is available at:
+#     http://www.ultrazed.org/product/ultrazed
+# 
+#  Disclaimer:
+#     Avnet, Inc. makes no warranty for the use of this code or design.
+#     This code is provided  "As Is". Avnet, Inc assumes no responsibility for
+#     any errors, which may appear in this code, nor does it make a commitment
+#     to update the information contained herein. Avnet, Inc specifically
+#     disclaims any implied warranties of fitness for a particular purpose.
+#                      Copyright(c) 2016 Avnet, Inc.
+#                              All rights reserved.
+# 
+# ----------------------------------------------------------------------------
+# 
+#  Create Date:         Dec 21, 2016
+#  Design Name:         UltraZed + PCIe Carrier
+#  Module Name:         uz_pciec_factest.tcl
+#  Project Name:        UltraZed PCIe Carrier HW Factory Acceptance Test
+#  Target Devices:      Xilinx Zynq UltraScale+ 3EG
+#  Hardware Boards:     UltraZed SOM + PCIe Carrier
+# 
+#  Tool versions:       Vivado 2016.2
+# 
+#  Description:         Build Script for UltraZed PCIe Carrier HW Factory  
+#                       Acceptance Test design 
+# 
+#  Dependencies:        To be called from a configured make script call
+#                       Calls support scripts, such as board configuration 
+#                       scripts IP generation scripts or others as needed
+# 
+#
+#  Revision:            Dec 21, 2016: 1.00 Initial version
+# 
+# ----------------------------------------------------------------------------
+
+# 'private' used to allow this project to be privately tagged
+# 'public' used to allow this project to be publicly tagged
+set release_state public
+
+if {[string match -nocase "yes" $clean]} {
+   # Clean up project output products.
+
+   # Open the existing project.
+   puts "***** Opening Vivado Project $projects_folder/$project.xpr ..."
+   open_project $projects_folder/$project.xpr
+   
+   # Reset output products.
+   reset_target all [get_files ${projects_folder}/${project}.srcs/sources_1/bd/${project}/${project}.bd]
+
+   # Reset design runs.
+   reset_run impl_1
+   reset_run synth_1
+
+   # Reset project.
+   reset_project
+} else {
+# Create Vivado project
+puts "***** Creating Vivado Project..."
+source ../Boards/$board/$board.tcl -notrace
+avnet_create_project $project $projects_folder $scriptdir
+
+# Apply board specific project property settings
+switch -nocase $board {
+   UZ3EG_PCIECC {
+      puts "***** Assigning Vivado Project board_part Property to ultrazed_3eg_pciecc..."
+      set_property board_part em.avnet.com:ultrazed_3eg_pciecc:part0:1.0 [current_project]
+   }
+
+}
+
+# Create Block Design and Add PS core
+puts "***** Creating Block Design..."
+create_bd_design ${project}
+set design_name ${project}
+avnet_add_ps_preset $project $projects_folder $scriptdir
+
+# General Config
+puts "***** General Configuration for Design..."
+set_property target_language VHDL [current_project]
+
+# Add the constraints that are needed for testing
+add_files -fileset constrs_1 -norecurse ${projects_folder}/../uz_pciec_factest.xdc
+
+# Create Block Diagram
+set design_name ${project}
+source ${projects_folder}/../../../Scripts/ProjectScripts/${project}_bd.tcl
+
+# Add Project source files
+puts "***** Adding Source Files to Block Design..."
+make_wrapper -files [get_files ${projects_folder}/${project}.srcs/sources_1/bd/${project}/${project}.bd] -top
+add_files -norecurse ${projects_folder}/${project}.srcs/sources_1/bd/${project}/hdl/${project}_wrapper.vhd
+
+# Build the binary
+#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+#*- KEEP OUT, do not touch this section unless you know what you are doing! -*
+#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+puts "***** Building Binary..."
+# add this to allow up+enter rebuild capability 
+cd $scripts_folder
+update_compile_order -fileset sources_1
+update_compile_order -fileset sim_1
+save_bd_design
+launch_runs impl_1 -to_step write_bitstream -j 8
+#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+#*- KEEP OUT, do not touch this section unless you know what you are doing! -*
+#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+}
