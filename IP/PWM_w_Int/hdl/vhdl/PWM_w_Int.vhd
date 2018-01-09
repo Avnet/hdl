ca86b6a2cfa23b1d9476698fa3c3d2acd2516f5a
zedhed <kevin.keryk@avnet.com>
2018-01-08 18:20:34 -0700

    Modified minized_speedway platform to adjust PWM_w_Int IP block output from 8 channel LEDs to 1 to accommodate MiniZed hardware and adjusted AXI GPIO output to single channel LED.

--- IP/PWM_w_Int/hdl/vhdl/PWM_w_Int.vhd
+++ IP/PWM_w_Int/hdl/vhdl/PWM_w_Int.vhd
@@ -16,7 +16,7 @@ entity PWM_w_Int is
 	);
 	port (
 		-- Users to add ports here
-           LEDs : out std_logic_vector( 7 downto 0);
+           LEDs : out std_logic_vector( 0 downto 0);
            Interrupt_Out : out std_logic;
            PWM_Counter: out std_logic_vector(PWM_PERIOD-1 downto 0);
            DutyCycle : out std_logic_vector(31 downto 0);
@@ -60,7 +60,7 @@ architecture arch_imp of PWM_w_Int is
 	      Clk : in std_logic;
 	      DutyCycle : in std_logic_vector(31 downto 0);
 	      Reset : in std_logic;
-	      PWM_out : out std_logic_vector(7 downto 0);
+	      PWM_out : out std_logic_vector(0 downto 0);
 	      Interrupt : out std_logic;
 	      count : out std_logic_vector(period-1 downto 0)
 	     );
