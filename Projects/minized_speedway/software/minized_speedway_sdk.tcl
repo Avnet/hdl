a233769d5162a99cd2fcd1eed02d45981ec062f2
zedhed <kevin.keryk@avnet.com>
2018-01-10 17:28:47 -0700

    Updated minized_speedway project to include LED_Dimmer_Int to test out hardware platform which includes Custom IP developed during course.  Also updated SDK TCL script to replace deprecated XSCT commands and also to adjust BSP to use UART1 for stdin and stdout to match MiniZed board configuration.

--- Projects/minized_speedway/software/minized_speedway_sdk.tcl
+++ Projects/minized_speedway/software/minized_speedway_sdk.tcl
@@ -1,11 +1,15 @@
 # ----------------------------------------------------------------------------
-#       _____
-#      *     *
-#     *____   *____
-#    * *===*   *==*
-#   *___*===*___**  AVNET
-#        *======*
-#         *====*
+#
+#        ** **        **          **  ****      **  **********  ********** ®
+#       **   **        **        **   ** **     **  **              **
+#      **     **        **      **    **  **    **  **              **
+#     **       **        **    **     **   **   **  *********       **
+#    **         **        **  **      **    **  **  **              **
+#   **           **        ****       **     ** **  **              **
+#  **  .........  **        **        **      ****  **********      **
+#     ...........
+#                                     Reach Further™
+#
 # ----------------------------------------------------------------------------
 # 
 #  This design is the property of Avnet.  Publication of this
@@ -40,37 +44,140 @@
 #  Dependencies:        To be called from a configured make script call
 #
 #  Revision:            Aug 14, 2017: 1.00 Initial version
+#                       Jan 10, 2018: 1.01 Added configuration parameters so
+#                                          that UART can be chagned from 
+#                                          default setting in the BSP which is
+#                                          useful for MiniZed projects also
+#                                          updated deprecated XSCT commands to
+#                                          latest commands as specified in
+#                                          most recent Xilinx doucmentation 
+#                                          UG1208
 # 
 # ----------------------------------------------------------------------------
 
 #!/usr/bin/tclsh
-set project  "minized_speedway"
-set hw_name  "minized_speedway_hw"
-set bsp_name "minized_speedway_bsp"
+
+##############################################################################
+#
+# Begin SDK project configuration parameters section used for the remainder 
+# of this script.
+#
+##############################################################################
+
+# This is the project name and should match the name of the Vivado HW project 
+# folder.
+set project                  "minized_speedway"
+
+# This is the Hardware Definition and will contain the exported HDF file from
+# the Vivado HW project and can be named anything but for historical purposes
+# you should consider naming it "${project}_hw" to keep it simple.
+set hw_name                  "${project}_hw"
+
+# This is the Board Support Package name and will contain the generated BSP 
+# based upon the Vivado HW project.  Although it can be named anything you 
+# fancy, for historical purposes consider naming it "${project}_bsp" to keep 
+# it simple. It is NOT to be confused with the BSP that gets generated for the
+# FSBL which should have its own BSP due to library dependencies upon xilffs 
+# and xilrsa libraries which may not be desired in the application BSP.
+set bsp_name                 "${project}_bsp"
+
+# This is the Standalone application that tests the LED_w_Int AXI IP instance
+# which proves that the Custom IP functions as expected from a processor 
+# perspective.
+set led_dimmer_test_app_name "LED_Dimmer_Int"
+
+# For project created by people who like to use UARTs other than ps7_uart_0, 
+# we have the following option now available which will be set for the BSPs
+# that get generated.
+set uart_port_name           "ps7_uart_1"
+
+# The name of the FSBL application that is created from the Zynq FSBL 
+# application template.
+set fsbl_app_name            "zynq_fsbl_app"
+
+# The name of the FSBL application that is generated.
+set fsbl_bsp_name            "zynq_fsbl_bsp"
+
+##############################################################################
+#
+# End SDK project configuration parameters section.
+#
+##############################################################################
+
+
+##############################################################################
+#
+# Create the Hardware Platform Specification project which is derived from
+# the Hardware Defininition File exported by Vivado.
+#
+##############################################################################
 
 # Set workspace and import hardware platform
-sdk set_workspace ${project}.sdk
+setws ${project}.sdk
 puts "\n#\n#\n# Importing hardware definition ${hw_name} from impl_1 folder ...\n#\n#\n"
 file copy -force ${project}.runs/impl_1/${project}_wrapper.sysdef ${project}.sdk/${hw_name}.hdf
 puts "\n#\n#\n# Create hardware definition project ...\n#\n#\n"
-sdk createhw -name ${hw_name} -hwspec ${project}.sdk/${hw_name}.hdf
+createhw -name ${hw_name} -hwspec ${project}.sdk/${hw_name}.hdf
 
-# Generate BSP
-puts "\n#\n#\n# Creating ${bsp_name} ...\n#\n#\n"
-sdk createbsp -name ${bsp_name} -proc ps7_cortexa9_0 -hwproject ${hw_name} -os standalone
-sdk projects -type bsp -name ${bsp_name}
+##############################################################################
+#
+# Create the Board Support Package project which is derived from the Hardware
+# Platform Specification project.
+#
+##############################################################################
+
+# Create the BSP project
+puts "\n#\n#\n# Creating BSP ${bsp_name} project ...\n#\n#\n"
+createbsp -name ${bsp_name} -proc ps7_cortexa9_0 -hwproject ${hw_name} -os standalone
+
+# Update BSP settings for standalone application.
+puts "\n#\n#\n# Configuring MSS file for BSP ${bsp_name} ...\n#\n#\n"
+configbsp -bsp ${project}.sdk/${bsp_name}/system.mss stdin ${uart_port_name}
+configbsp -bsp ${project}.sdk/${bsp_name}/system.mss stdout ${uart_port_name}
+updatemss -mss ${project}.sdk/${bsp_name}/system.mss
+regenbsp -bsp ${bsp_name}
+
+# Build the standalone BSP needed for applications
+puts "\n#\n#\n# Building BSP ${bsp_name} project ...\n#\n#\n"
+sdk projects -build -type bsp -name ${bsp_name}
+
+##############################################################################
+#
+# Create the Standalone applications which depend upon the Board Support 
+# Package project.
+#
+##############################################################################
+
+# Create LED Dimmer Test application
+puts "\n#\n#\n# Creating ${led_dimmer_test_app_name} ...\n#\n#\n"
+createapp -name ${led_dimmer_test_app_name} -hwproject ${hw_name} -proc ps7_cortexa9_0 -os standalone -lang C -app {Empty Application} -bsp ${bsp_name} 
+
+# Import revision controlled source code into the application projects
+puts "\n#\n#\n# Importing application code into projects ...\n#\n#\n"
+importsources -name ${led_dimmer_test_app_name} -path ../software/${led_dimmer_test_app_name}/src
+
+# Build LED Dimmer Test application
+puts "\n#\n#\n# Building APP ${led_dimmer_test_app_name} ...\n#\n#\n"
+projects -build -type app -name ${led_dimmer_test_app_name}
+
+##############################################################################
+#
+# Create the First Stage Boot Loader (FSBL) project which will be used to
+# create boot images for Standalone applications.
+#
+##############################################################################
 
 # Create FSBL application
-puts "\n#\n#\n# Creating zynq_fsbl ...\n#\n#\n"
-sdk createapp -name zynq_fsbl_app -hwproject ${hw_name} -proc ps7_cortexa9_0 -os standalone -lang C -app {Zynq FSBL} -bsp zynq_fsbl_bsp
+puts "\n#\n#\n# Creating Zynq FSBL project ${fsbl_app_name} ...\n#\n#\n"
+createapp -name ${fsbl_app_name} -hwproject ${hw_name} -proc ps7_cortexa9_0 -os standalone -lang C -app {Zynq FSBL} -bsp ${fsbl_bsp_name}
 
 # Set the build type to release
-configapp -app zynq_fsbl_app build-config release
+configapp -app ${fsbl_app_name} build-config release
 
 # Build FSBL application
-puts "\n#\n#\n Building zynq_fsbl ...\n#\n#\n"
-sdk build_project -type bsp -name zynq_fsbl_bsp
-sdk build_project -type app -name zynq_fsbl_app
+puts "\n#\n#\n# Building FSBL APP ${fsbl_app_name} ...\n#\n#\n"
+projects -build -type bsp -name ${fsbl_bsp_name}
+projects -build -type app -name ${fsbl_app_name}
 
 # done
 exit
