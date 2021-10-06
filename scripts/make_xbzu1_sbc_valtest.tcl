4617e1bc7a38131b5dd300c5b5df67d7efe7921e
Tom Curran <tom.curran@avnet.com>
2021-10-06 07:31:39 -0400

    Add valtest project for XB-ZU1 board.

---
+++ scripts/make_xbzu1_sbc_valtest.tcl
@@ -0,0 +1,55 @@
+# ----------------------------------------------------------------------------
+#
+#        ** **        **          **  ****      **  **********  ********** ®
+#       **   **        **        **   ** **     **  **              **
+#      **     **        **      **    **  **    **  **              **
+#     **       **        **    **     **   **   **  *********       **
+#    **         **        **  **      **    **  **  **              **
+#   **           **        ****       **     ** **  **              **
+#  **  .........  **        **        **      ****  **********      **
+#     ...........
+#                                     Reach Further™
+#
+# ----------------------------------------------------------------------------
+#
+#  This design is the property of Avnet.  Publication of this
+#  design is not authorized without written consent from Avnet.
+#
+#  Please direct any questions to the UltraZed community support forum:
+#     http://avnet.me/<TBD>
+#
+#  Product information is available at:
+#     http://avnet.me/<TBD>
+#
+#  Disclaimer:
+#     Avnet, Inc. makes no warranty for the use of this code or design.
+#     This code is provided  "As Is". Avnet, Inc assumes no responsibility for
+#     any errors, which may appear in this code, nor does it make a commitment
+#     to update the information contained herein. Avnet, Inc specifically
+#     disclaims any implied warranties of fitness for a particular purpose.
+#                      Copyright(c) 2021 Avnet, Inc.
+#                              All rights reserved.
+#
+# ----------------------------------------------------------------------------
+#
+#  Create Date:         Oct 05, 2021
+#  Design Name:         XBoard-ZU1 Validation Test HW Platform
+#  Module Name:         make_xbzu1_sbc_valtest.tcl
+#  Project Name:        XBoard-ZU1 Validation Test HW
+#  Target Devices:      Xilinx Zynq UltraScale+ 1CG
+#  Hardware Boards:     Xboard-ZU1 Board
+#
+# ----------------------------------------------------------------------------
+
+if {$argc != 0} {
+	# Build valtest hw platform
+	set argv [list board=[lindex $argv 0] project=[lindex $argv 1] sdk=no close_project=yes dev_arch=zynqmp]
+	set argc [llength $argv]
+	source ./make.tcl -notrace
+} else {
+	# Build valtest hw platform
+   set argv [list board=xbzu1_sbc project=valtest sdk=no close_project=yes dev_arch=zynqmp]
+   set argc [llength $argv]
+   source ./make.tcl -notrace
+}
+
