4464da9cdb7551f156feed21ea7dcd7317d99830
Tom Curran <tom.curran@avnet.com>
2022-04-14 14:47:02 -0400

    New zub1cg factory test hw platform

---
+++ scripts/make_zub1cg_sbc_factest.tcl
@@ -0,0 +1,55 @@
+# ----------------------------------------------------------------------------
+#
+#        ** **        **          **  ****      **  **********  ********** ®
+#       **   **        **        **   ** **     **  **              **
+#      **     **        **      **    **  **    **  **              **
+#     **       **        **    **     **   **   **  *********       **
+#    **         **        **  **      **    **  **  **              **
+#   **           **        ****       **     ** **  **              **
+#  **  .........  **        **        **      ****  **********      **
+#     ...........
+#                                     Reach Further™
+#
+# ----------------------------------------------------------------------------
+#
+#  This design is the property of Avnet.  Publication of this
+#  design is not authorized without written consent from Avnet.
+#
+#  Please direct any questions to the UltraZed community support forum:
+#     http://avnet.me/<TBD>
+#
+#  Product information is available at:
+#     http://avnet.me/<TBD>
+#
+#  Disclaimer:
+#     Avnet, Inc. makes no warranty for the use of this code or design.
+#     This code is provided  "As Is". Avnet, Inc assumes no responsibility for
+#     any errors, which may appear in this code, nor does it make a commitment
+#     to update the information contained herein. Avnet, Inc specifically
+#     disclaims any implied warranties of fitness for a particular purpose.
+#                      Copyright(c) 2021 Avnet, Inc.
+#                              All rights reserved.
+#
+# ----------------------------------------------------------------------------
+#
+#  Create Date:         Apr 08, 2022
+#  Design Name:         ZUBoard-1CG Factory Test HW Platform
+#  Module Name:         make_zub1cg_sbc_factest.tcl
+#  Project Name:        ZUBoard-1CG Factory Test HW
+#  Target Devices:      Xilinx Zynq UltraScale+ 1CG
+#  Hardware Boards:     Xboard-ZU1 Board
+#
+# ----------------------------------------------------------------------------
+
+if {$argc != 0} {
+	# Build factest hw platform
+	set argv [list board=[lindex $argv 0] project=[lindex $argv 1] sdk=no close_project=yes dev_arch=zynqmp]
+	set argc [llength $argv]
+	source ./make.tcl -notrace
+} else {
+	# Build factest hw platform
+   set argv [list board=zub1cg_sbc project=factest sdk=no close_project=yes dev_arch=zynqmp]
+   set argc [llength $argv]
+   source ./make.tcl -notrace
+}
+
