6fb5fd9edec621e6ea3fc059edf5e240b917853e
Bruce Thomson <bthomson@witekio.com>
2022-09-28 14:43:49 +0100

    Update PS IP for zub1cg out-of-box vivado project

--- boards/zub1cg_sbc/oob/zub1cg_sbc_oob.tcl
+++ boards/zub1cg_sbc/oob/zub1cg_sbc_oob.tcl
@@ -15,11 +15,8 @@
 #  This design is the property of Avnet.  Publication of this
 #  design is not authorized without written consent from Avnet.
 #
-#  Please direct any questions to the community support forum:
-#     http://avnet.me/TBD
-#
 #  Product information is available at:
-#     http://avnet.me/TBD
+#     http://avnet.me/zuboard-1cg
 #
 #  Disclaimer:
 #     Avnet, Inc. makes no warranty for the use of this code or design.
@@ -27,7 +24,7 @@
 #     any errors, which may appear in this code, nor does it make a commitment
 #     to update the information contained herein. Avnet, Inc specifically
 #     disclaims any implied warranties of fitness for a particular purpose.
-#                      Copyright(c) 2021 Avnet, Inc.
+#                      Copyright(c) 2022 Avnet, Inc.
 #                              All rights reserved.
 #
 # ----------------------------------------------------------------------------
@@ -595,7 +592,7 @@ proc avnet_add_user_io_preset {project projects_folder scriptdir} {
 proc avnet_add_ps_preset {project projects_folder scriptdir} {
 
    # add selection for customization depending on board choice (or none)
-   create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.3 zynq_ultra_ps_e_0
+   create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.4 zynq_ultra_ps_e_0
    apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1" } [get_bd_cells zynq_ultra_ps_e_0]
    set zynq_ultra_ps_e_0 [get_bd_cells zynq_ultra_ps_e_0]
 
