4ae4bb032b5c769753f550e9e4d5b471e27c2f23
Thomas Nizan <tnizan@witekio.com>
2024-04-23 15:12:26 -0700

    boards: zub1cg_sbc: trifecta: update IPs to 2023.2

--- boards/zub1cg_sbc/trifecta/zub1cg_sbc_trifecta.tcl
+++ boards/zub1cg_sbc/trifecta/zub1cg_sbc_trifecta.tcl
@@ -667,7 +667,7 @@ proc create_hier_cell_CAPTURE_PIPELINE { parentCell nameHier } {
       CONFIG.TUSER_REMAP {tuser[0:0]}] [get_bd_cells axis_subset_converter_1]
 
    # Create instance: mipi_csi2_rx_subsyst_0, and set properties
-   create_bd_cell -type ip -vlnv xilinx.com:ip:mipi_csi2_rx_subsystem:5.3 mipi_csi2_rx_subsyst_0
+   create_bd_cell -type ip -vlnv xilinx.com:ip:mipi_csi2_rx_subsystem:5.4 mipi_csi2_rx_subsyst_0
    set_property -dict [ list \
       CONFIG.AXIS_TDEST_WIDTH {4} \
       CONFIG.CLK_LANE_IO_LOC {N2} \
@@ -717,7 +717,7 @@ proc create_hier_cell_CAPTURE_PIPELINE { parentCell nameHier } {
    create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 proc_sys_reset_3
 
    # Create instance: v_frmbuf_wr_0, and set properties
-   create_bd_cell -type ip -vlnv xilinx.com:ip:v_frmbuf_wr:2.4 v_frmbuf_wr_0
+   create_bd_cell -type ip -vlnv xilinx.com:ip:v_frmbuf_wr:2.5 v_frmbuf_wr_0
    set_property -dict [ list \
       CONFIG.AXIMM_DATA_WIDTH {128} \
       CONFIG.C_M_AXI_MM_VIDEO_DATA_WIDTH {128} \
